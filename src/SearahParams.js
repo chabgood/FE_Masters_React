85b083d73d2daff87e649c551be97e969ff1b708
Chris Habgood <chabgood@gmail.com>
2020-03-01 01:29:59 -0600

    pet search

--- src/SearahParams.js
+++ src/SearahParams.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropDown from "./useDropDown";
 
 const SearchParams = () => {
-  const [location, setlocation] = useState("Austin, TX");
+  const [location, setlocation] = useState("Seattle, WA");
   const [breeds, setbreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropDown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setbreed] = useDropDown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
 
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+    setPets(animals || []);
+  }
   useEffect(() => {
     setbreed("");
     setbreeds([]);
@@ -19,7 +29,12 @@ const SearchParams = () => {
   }, [animal, setbreeds, setbreed]);
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           location
           <input
@@ -33,6 +48,7 @@ const SearchParams = () => {
           <button>Submit</button>
         </label>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
